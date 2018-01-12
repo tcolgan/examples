f8725b2686f7c205b4810e27cc3eb711d7c79d3c
tcolgan <tcolgan001@hotmail.com>
2018-01-12 09:34:45 -0500

    adding sandbox project

---
+++ cpp/sandbox/src/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main( int argc , char **argv ) 
+{
+  printf( "this is a test.\n") ;
+
+  for( int idx = 0 ; idx < 5 ; idx++ ) 
+  {
+    printf( "%d\n" , idx ) ;
+
+  }
+}
+
