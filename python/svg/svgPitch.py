89e63281c6eb32507d51fcc7c3e00d4a4ae4821e
Tim Colgan <tcolgan001@hotmail.com>
2022-08-16 11:38:05 -0400

    working on arpeggios

--- python/svg/svgPitch.py
+++ python/svg/svgPitch.py
@@ -17,103 +17,90 @@ class svgPitch(svgGroup):
 
     super().__init__()
         
-    self._pitch = pitch
     self._xloc = xloc
     self._yloc = yloc
-    self._pitchIcon = svgGroup(scaleX=scale,scaleY=scale)
 
 
-    if self._pitch == 0 :
-      self.addSquare(xloc , yloc, ICON_SIZE)
+    if pitch == 0 :
+      self.addSquare(ICON_SIZE)
         
-    elif self._pitch == 1 :
-      self.addX( xloc , yloc )
-      self.addCircle(xloc,yloc,radius=ICON_SIZE_HALF/2,colorFill="black")
+    elif pitch == 1 :
+      self.addX()
+      self.addCircle(radius=ICON_SIZE_HALF/2,colorFill="black")
 
-    elif self._pitch == 2 :
-      self.addX( xloc , yloc )
+    elif pitch == 2 :
+      self.addX()
 
-    elif self._pitch == 3 :
-      self.addTriangle(xloc,yloc,colorFill="black")
+    elif pitch == 3 :
+      self.addTriangle(colorFill="black")
         
-    elif self._pitch == 4 :
-      self.addTriangle(xloc,yloc,colorFill="white")
+    elif pitch == 4 :
+      self.addTriangle(colorFill="white")
 
-    elif self._pitch == 5 :
-      self.addDiamond(xloc,yloc)
+    elif pitch == 5 :
+      self.addDiamond()
 
-    elif self._pitch == 6 :
-      self.addCircle(xloc,yloc,radius=ICON_SIZE_HALF,colorFill="black")
+    elif pitch == 6 :
+      self.addCircle(radius=ICON_SIZE_HALF,colorFill="black")
 
-    elif self._pitch == 7 :
-      self.addCircle(xloc,yloc,radius=ICON_SIZE_HALF,colorFill="white")
+    elif pitch == 7 :
+      self.addCircle(radius=ICON_SIZE_HALF,colorFill="white")
 
-    elif self._pitch == 8 :
-      self.add6( xloc , yloc )
-      self.addCircle(xloc,yloc,radius=ICON_SIZE_HALF/2,colorFill="black")
+    elif pitch == 8 :
+      self.add6()
+      self.addCircle(radius=ICON_SIZE_HALF/2,colorFill="black")
 
-    elif self._pitch == 9 :
-      self.add6( xloc , yloc )
+    elif pitch == 9 :
+      self.add6()
 
-    elif self._pitch == 10 :
-      self.addCircle(xloc,yloc,radius=ICON_SIZE_HALF,colorFill="black")
+    elif pitch == 10 :
+      self.addCircle(radius=ICON_SIZE_HALF,colorFill="black")
 
-    elif self._pitch == 11 :
-      self.addCircle(xloc,yloc,radius=ICON_SIZE_HALF,colorFill="grey")
+    elif pitch == 11 :
+      self.addCircle(radius=ICON_SIZE_HALF,colorFill="white")
 
         
-  def draw(self):
-    self._pitchIcon.draw()
-        
-        
-  def addX( self , xloc , yloc ):
-    self._pitchIcon.add( svgLine( xloc + ICON_SIZE_HALF , yloc + ICON_SIZE_HALF , xloc - ICON_SIZE_HALF , yloc - ICON_SIZE_HALF) )
-    self._pitchIcon.add( svgLine( xloc - ICON_SIZE_HALF , yloc + ICON_SIZE_HALF , xloc + ICON_SIZE_HALF , yloc - ICON_SIZE_HALF) )
+  def addX( self ):
+    self.add( svgLine( +ICON_SIZE_HALF , +ICON_SIZE_HALF , -ICON_SIZE_HALF , -ICON_SIZE_HALF) )
+    self.add( svgLine( -ICON_SIZE_HALF , +ICON_SIZE_HALF , +ICON_SIZE_HALF , -ICON_SIZE_HALF) )
     
     
-  def add6( self , xloc , yloc ):
+  def add6( self ):
     x_delta = ICON_SIZE_HALF * math.cos( 2 * math.pi * 30 / 360 )
     y_delta = ICON_SIZE_HALF * math.sin( 2 * math.pi * 30 / 360 )
 
-    self._pitchIcon = svgGroup()
-    self._pitchIcon.add( svgLine( xloc , yloc + ICON_SIZE_HALF , xloc , yloc - ICON_SIZE_HALF ) )
-    self._pitchIcon.add( svgLine( xloc + x_delta , yloc + y_delta , xloc - x_delta , yloc - y_delta ) )
-    self._pitchIcon.add( svgLine( xloc - x_delta , yloc + y_delta , xloc + x_delta , yloc -   y_delta ) )
+    self.add( svgLine( 0        , +ICON_SIZE_HALF , 0        , -ICON_SIZE_HALF ) )
+    self.add( svgLine( +x_delta , +y_delta        , -x_delta , -y_delta ) )
+    self.add( svgLine( -x_delta , +y_delta        , +x_delta , -y_delta ) )
 
 
-  def addLittleCircle(self,xloc,yloc):
+  def addDiamond(self):
     
-    radius = ICON_SIZE_HALF / 2
-    self._pitchIcon.add( svgEllipse( xloc , yloc , radius , radius , colorFill="black") )
+    self.add( svgLine( +ICON_SIZE_HALF , 0 , 0 , +ICON_SIZE_HALF) )
+    self.add( svgLine( -ICON_SIZE_HALF , 0 , 0 , +ICON_SIZE_HALF) )
+    self.add( svgLine( -ICON_SIZE_HALF , 0 , 0 , -ICON_SIZE_HALF) )
+    self.add( svgLine( +ICON_SIZE_HALF , 0 , 0 , -ICON_SIZE_HALF) )
 
 
-  def addDiamond(self,xloc,yloc):
-    
-    self._pitchIcon.add( svgLine( xloc + ICON_SIZE_HALF , yloc , xloc , yloc + ICON_SIZE_HALF) )
-    self._pitchIcon.add( svgLine( xloc - ICON_SIZE_HALF , yloc , xloc , yloc + ICON_SIZE_HALF) )
-    self._pitchIcon.add( svgLine( xloc - ICON_SIZE_HALF , yloc , xloc , yloc - ICON_SIZE_HALF) )
-    self._pitchIcon.add( svgLine( xloc + ICON_SIZE_HALF , yloc , xloc , yloc - ICON_SIZE_HALF) )
-
-
-  def addTriangle(self,xloc,yloc,colorFill="white"):
+  def addTriangle(self,colorFill="white"):
 
     x_delta = ICON_SIZE_HALF + math.cos( 2 * math.pi * 30 / 360 )
     y_delta = ICON_SIZE_HALF + math.sin( 2 * math.pi * 30 / 360 )
 
-    x1 = xloc
-    y1 = yloc - ICON_SIZE_HALF
-    x2 = xloc + x_delta
-    y2 = yloc + y_delta
-    x3 = xloc - x_delta
-    y3 = yloc + y_delta
+    x1 = 0
+    y1 = -ICON_SIZE_HALF
+    x2 = x_delta
+    y2 = y_delta
+    x3 = -x_delta
+    y3 = y_delta
     
     points = [(x1,y1),(x2,y2),(x3,y3)]
-    self._pitchIcon.add( svgPolygon( points , colorFill ) )
+    self.add( svgPolygon( points , colorFill ) )
     
 
-  def addSquare(self,xloc,yloc,size):
-    self._pitchIcon.add( svgRect( size , size , xloc - size / 2 , yloc - size / 2 ) )
+  def addSquare(self,size):
+    self.add( svgRect( size , size , -size / 2 , -size / 2 ) )
 
 
-  def addCircle(self,xloc,yloc,radius,colorFill="white"):
-    self._pitchIcon.add( svgEllipse( xloc , yloc , radius , radius, colorFill=colorFill ) )
+  def addCircle(self,radius,colorFill="white"):
+    self.add( svgEllipse( 0 , 0 , radius , radius, colorFill=colorFill ) )
