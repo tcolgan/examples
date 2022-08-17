51fed41faa06706e7287e51ab3d06fa88ef15cc1
Tim Colgan <tcolgan001@hotmail.com>
2022-08-17 10:42:28 -0400

    working on arpeggios

--- python/svg/arpeggios.py
+++ python/svg/arpeggios.py
@@ -17,6 +17,14 @@ HEIGHT_FONT_12 = 12 * FONT_TO_PIXELS
 
 VERTICAL_OFFSET = 1.2 * PIXELS_PER_INCH
 
+NUMBER_FRETS = 20
+
+NUMBER_STRINGS = 4
+NUMBER_FRETS = 20
+
+NUMBER_NOTES = 12
+
+
 width  =  8.5 * PIXELS_PER_INCH - 2 * HBORDER
 height = 11.0 * PIXELS_PER_INCH - 2 * VBORDER
 
@@ -42,12 +50,48 @@ pitch_arrays = [
     ( "Xsus2"           , [0,2,7]    ),
     ( "Xsus4"           , [0,5,7]    ),
   ]
+  
+note_matrix = [
+    [ -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 ] ,
+    [ -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 ] ,
+    [ -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 ] ,
+    [ -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 , -1 ] ,
+  ]
+  
+  
+def incr( start ) :
+  value = start + 1
+  if value >= NUMBER_NOTES :
+    value = value - NUMBER_NOTES
+  return value
+
+
+def makeNoteMatrix( pitches ) :
+  matrix = []
+
+  pitch_string = [9,2,7,0]
+  for string in range( NUMBER_STRINGS ) :
+    array = []
+    pitch = pitch_string[ string ]
+    for fret in range( NUMBER_FRETS ) :
+      if pitch in pitches:
+        val = pitch
+      else:
+        val = -1
+      array.append( val )
+      pitch = incr( pitch )
+    matrix.append( array )
+  return matrix
+  
 
 for text,pitches in pitch_arrays:
   
   dwg.add( svgConstellation( diameter , xloc , yloc , pitches  ))
-  dwg.add( svgFretboard( xloc + 2*diameter , yloc - diameter , pitches ))
+  dwg.add( svgFretboard( xloc + 2*diameter , yloc - diameter , NUMBER_FRETS , makeNoteMatrix( pitches ) ))
   dwg.add( svgText( xloc - text_offset , yloc , text , rotate=270.0) )
   yloc += VERTICAL_OFFSET
   
 dwg.draw()
+
+
+      
--- python/svg/svgFretboard.py
+++ python/svg/svgFretboard.py
@@ -19,7 +19,7 @@ DOT_RADIUS = 3
 
 class svgFretboard(svgGroup):
   
-  def __init__(self,xloc,yloc,pitches=[]):
+  def __init__(self,xloc,yloc , number_frets , note_matrix = [[],[],[],[]] ):
 
     super().__init__()
 
@@ -27,27 +27,27 @@ class svgFretboard(svgGroup):
     self._yloc = yloc
 
     
-    width  = NUMBER_FRETS * DEFAULT_FRET_SEPARATION
+    width  = number_frets * DEFAULT_FRET_SEPARATION
     height = ( NUMBER_STRINGS - 1 ) * DEFAULT_STRING_SEPARATION
 
     yloc = 0
     pitch_start = [9,2,7,0]
     dots = [0,0,0,1,0,1,0,1,0,0,1,0,2,0,0,1,0,1,0,1,0,0]
     for string in range( NUMBER_STRINGS ):
+      notes = note_matrix[ string ]
       pitch_idx = pitch_start[ string ]
       self.add( svgLine(  0 , yloc , width , yloc ) )
       xloc = DEFAULT_FRET_SEPARATION
-      for fret in range( NUMBER_FRETS ) :
-        if fret != NUMBER_FRETS - 1 :
+      for fret in range( number_frets ) :
+        note = notes[ fret ]
+        if fret != number_frets - 1 :
           self.add( svgLine(  xloc , 0 , xloc , height ) )
-        if pitch_idx in pitches:
-          self.add( svgPitch( xloc - DEFAULT_FRET_SEPARATION / 2 , yloc , pitch_idx ))
-        pitch_idx = self.incr( pitch_idx )
+        if note >= 0 :
+          self.add( svgPitch( xloc - DEFAULT_FRET_SEPARATION / 2 , yloc , note ))
+
         if string == 0 :
-          
           if dots[fret] == 1 :
             self.addCircle( DOT_RADIUS , xloc - DEFAULT_FRET_SEPARATION / 2 , yloc + height + DEFAULT_STRING_SEPARATION * .75 )
-            
           elif dots[fret] == 2 :
             self.addCircle( DOT_RADIUS , xloc - DEFAULT_FRET_SEPARATION / 2 , yloc + height + DEFAULT_STRING_SEPARATION * .5 )
             self.addCircle( DOT_RADIUS , xloc - DEFAULT_FRET_SEPARATION / 2 , yloc + height + DEFAULT_STRING_SEPARATION * 1.0 )
@@ -65,7 +65,4 @@ class svgFretboard(svgGroup):
     if value >= NUMBER_NOTES :
       value = value - NUMBER_NOTES
     return value
-        
-#  for pitch in pitches:
 
-#    self.add( svgPitch( xpitch , ypitch , pitch ) )
