1cafe68d73026fa5bf50cdc3813dbc81eb31ba3a
Tim Colgan <tcolgan001@hotmail.com>
2022-11-28 10:14:12 -0500

    fixed bug in pitchArrays.py for major pentatonic scale

--- python/svg/pitchArrays.py
+++ python/svg/pitchArrays.py
@@ -1,9 +1,9 @@
 pitch_arrays = {
     "X Chromatic Scale" : [0,1,2,3,4,5,6,7,8,9,10,11] ,
     "X Major Scale"     : [0,2,4,5,7,9,11]            ,
-    "X Major Pent Scale": [0,2,5,7,9]                 ,
+    "X Major Pent Scale": [0,2,4,7,9]                 ,
     "X Minor Scale"     : [0,2,3,5,7,8,10]            ,
-    "X Minor Pent Scale": [0,3,5,7,10]                 ,
+    "X Minor Pent Scale": [0,3,5,7,10]                ,
     "X7"                : [0,4,7,10]                  ,
     "Xmaj7"             : [0,4,7,11]                  ,
     "Xm7"               : [0,3,7,10]                  ,
